64d1ecaac16f22cb3c7c1d090af6e255fb792136
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-25 20:08:46 -0300

    Calculando a area de um triangulo com utilizando conceitos de orientação a objetos

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,34 +2,69 @@ package aplication;
 
 import java.util.Scanner;
 
+import entities.Triangle;
+
 
 // Solução de calcular a aréa de dois triangulos, sem a orientação a Objetos.
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
+//		Scanner sc = new Scanner(System.in);
+//		
+//
+//		Double xA, xB, xC, yA, yB, yC;
+//		
+//		System.out.println("Enter the measures of triangules X:");
+//		xA = sc.nextDouble();
+//		xB = sc .nextDouble();
+//		xC = sc.nextDouble();
+//		
+//		System.out.println("Enter the measures of triangules Y:");
+//		yA = sc.nextDouble();
+//		yB = sc .nextDouble();
+//		yC = sc.nextDouble();
+//
+//		double p = (xA + xB + xC) / 2.0;
+//		double areaX =  Math.sqrt( p * ( p -xA ) * ( p - xB) * (p -xC) );
+//		
+//		
+//		p = (yA + yB + yC) / 2.0;
+//		double areaY = Math.sqrt( p * (p - yA ) * (p - yB) * (p - yC) );
+//		
+//		System.out.printf("Triangulo X area: %.4f%n", areaX);
+//		System.out.printf("Triangulo Y area: %.4f%n", areaY);
+//		
+//		if(areaX > areaY) {
+//			System.out.println("Larger area: X");
+//		}else {
+//			System.out.println("Larger area: Y");
+//		}		
+//		sc.close();
 
-		Double xA, xB, xC, yA, yB, yC;
+		Scanner sc = new Scanner(System.in);	
+	
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangules X:");
-		xA = sc.nextDouble();
-		xB = sc .nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
-		System.out.println("Enter the measures of triangules Y:");
-		yA = sc.nextDouble();
-		yB = sc .nextDouble();
-		yC = sc.nextDouble();
 
-		double p = (xA + xB + xC) / 2.0;
-		double areaX =  Math.sqrt( p * ( p -xA ) * ( p - xB) * (p -xC) );
 		
+		double areaX =  x.area();
+			
+		System.out.println("Enter the measures of triangules Y:");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt( p * (p - yA ) * (p - yB) * (p - yC) );
-		
+
+		double areaY = y.area();		
+
 		System.out.printf("Triangulo X area: %.4f%n", areaX);
 		System.out.printf("Triangulo Y area: %.4f%n", areaY);
 		
@@ -37,13 +72,11 @@ public class Program {
 			System.out.println("Larger area: X");
 		}else {
 			System.out.println("Larger area: Y");
-		}
-				
-	
-				
-				sc.close();
-	
-				
+		}		
+
+		
+		sc.close();
+		
 				
 	}
 
