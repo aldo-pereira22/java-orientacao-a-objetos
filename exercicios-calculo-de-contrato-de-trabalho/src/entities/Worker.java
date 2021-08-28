4abd31dd6bcdf6b214e170612494c22e27af160b
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-28 11:01:11 -0300

    Resolução do exercicio de Calculo de contratos de trabalhadores parte-1

---
+++ exercicios-calculo-de-contrato-de-trabalho/src/entities/Worker.java
@@ -0,0 +1,112 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalaray;
+
+	private Department department;
+
+	private List<HourContract> contracts = new ArrayList<>();
+
+	public Worker() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSalaray, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalaray = baseSalaray;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalaray() {
+		return baseSalaray;
+	}
+
+	public void setBaseSalaray(Double baseSalaray) {
+		this.baseSalaray = baseSalaray;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	
+	
+	public double income(int year, int month) {
+		double sum = baseSalaray;
+		Calendar cal = Calendar.getInstance();
+		
+		for(HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1+ cal.get(Calendar.MONTH);
+			
+			if( year == c_year && month == c_month ) {
+				sum += c.totalValue();
+			}
+			
+		}
+		return sum;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
