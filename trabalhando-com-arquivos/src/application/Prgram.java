85188fbeb04e01949a337228c344521176a3024c
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-09-01 20:53:45 -0300

    leitura de arquivo

--- trabalhando-com-arquivos/src/application/Prgram.java
+++ trabalhando-com-arquivos/src/application/Prgram.java
@@ -1,30 +1,74 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Prgram {
 	public static void main(String[] args) {
 		
-		File file = new File("/home/byaldo/teste.txt");
-		Scanner sc =null;
+//		File file = new File("/home/byaldo/teste.txt");
+//		Scanner sc =null;
+//		
+//		try {
+//			sc = new Scanner(file);
+//			while(sc.hasNextLine()) {
+//				System.out.println(sc.nextLine());
+//			}
+//			
+//
+//		} catch (Exception e) {
+//			
+//			System.out.println();
+//		} finally {
+//			if( sc != null) {
+//				sc.close();
+//			}
+//		}
+		
+		
+		String path = "/home/byaldo/teste.txt";
+		
+		FileReader fr = null;
+		BufferedReader br = null;
 		
 		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while(line != null ) {
+				System.out.println(line);
+				line = br.readLine();
 			}
+		} catch (IOException e) {
+			System.out.println("Error: " +e.getMessage());
 			
-
-		} catch (Exception e) {
-			
-			System.out.println();
-		} finally {
-			if( sc != null) {
-				sc.close();
+		}finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+				
+				if(fr != null ) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
 			}
 		}
-		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
