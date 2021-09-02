6df3bb44ce14dba9577a458a6424634dd11bb7f6
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-09-02 20:50:45 -0300

    Interface parte-2

--- interface/src/model/entities/CarRental.java
+++ interface/src/model/entities/CarRental.java
@@ -21,6 +21,38 @@ public class CarRental {
 		this.vehicle = vehicle;
 	
 	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
 	
 	
 	
